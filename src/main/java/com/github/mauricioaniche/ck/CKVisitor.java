ee3991503137fb691f2817a20a778c4a77ad99de
João Pedro Ennes <joaopedroennes@outlook.com>
2024-05-26 17:53:29 -0300

    improve: Local variables should not shadow class fields

--- src/main/java/com/github/mauricioaniche/ck/CKVisitor.java
+++ src/main/java/com/github/mauricioaniche/ck/CKVisitor.java
@@ -73,13 +73,10 @@ public class CKVisitor extends ASTVisitor {
 				
 		}
 
-		// create a set of visitors, just for the current class
-		List<ClassLevelMetric> classLevelMetrics = instantiateClassLevelMetricVisitors(className);
-
 		// store everything in a 'class in the stack' data structure
 		ClassInTheStack classInTheStack = new ClassInTheStack();
 		classInTheStack.result = currentClass;
-		classInTheStack.classLevelMetrics = classLevelMetrics;
+		classInTheStack.classLevelMetrics = instantiateClassLevelMetricVisitors(className);
 
 		// push it to the stack, so we know the current class we are visiting
 		classes.push(classInTheStack);
@@ -127,7 +124,7 @@ public class CKVisitor extends ASTVisitor {
 		// we add it to the current class we are visiting
 		MethodInTheStack methodInTheStack = new MethodInTheStack();
 		methodInTheStack.result = currentMethod;
-		methodInTheStack.methodLevelMetrics = methodLevelMetrics;
+		methodInTheStack.methodLevelMetrics = instantiateMethodLevelMetricVisitors(currentQualifiedMethodName);
 		classes.peek().methods.push(methodInTheStack);
 
 		// and there might be metrics that also use the methoddeclaration node.
@@ -215,13 +212,10 @@ public class CKVisitor extends ASTVisitor {
 		currentMethod.setLoc(calculate(node.toString()));
 		currentMethod.setStartLine(JDTUtils.getStartLine(cu, node));
 
-		// let's instantiate method level visitors for this current method
-		List<MethodLevelMetric> methodLevelMetrics = instantiateMethodLevelMetricVisitors(currentMethodName);
-
 		// we add it to the current class we are visiting
 		MethodInTheStack methodInTheStack = new MethodInTheStack();
 		methodInTheStack.result = currentMethod;
-		methodInTheStack.methodLevelMetrics = methodLevelMetrics;
+		methodInTheStack.methodLevelMetrics = instantiateMethodLevelMetricVisitors(currentMethodName);
 		classes.peek().methods.push(methodInTheStack);
 
 		// and there might be metrics that also use the methoddeclaration node.
@@ -270,13 +264,10 @@ public class CKVisitor extends ASTVisitor {
 		CKClassResult currentClass = new CKClassResult(sourceFilePath, className, type, modifiers);
 		currentClass.setLoc(calculate(node.toString()));
 
-		// create a set of visitors, just for the current class
-		List<ClassLevelMetric> classLevelMetrics = instantiateClassLevelMetricVisitors(className);
-
 		// store everything in a 'class in the stack' data structure
 		ClassInTheStack classInTheStack = new ClassInTheStack();
 		classInTheStack.result = currentClass;
-		classInTheStack.classLevelMetrics = classLevelMetrics;
+		classInTheStack.classLevelMetrics = instantiateClassLevelMetricVisitors(className);
 
 		// push it to the stack, so we know the current class we are visiting
 		classes.push(classInTheStack);
